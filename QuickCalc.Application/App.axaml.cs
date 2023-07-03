68134d917f28a25cd72acd771054b97d1e5abfb2
wuxiaole <iamherrylok@gmail.com>
2023-07-03 08:40:50 +0800

    将MAUI改为Avalonia

---
+++ QuickCalc.Application/App.axaml.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+using QuickCalc.Application.ViewModels;
+using QuickCalc.Application.Views;
+
+namespace QuickCalc.Application;
+
+public partial class App : Avalonia.Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new Dashboard
+            {
+                DataContext = new DashboardViewModel()
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new Dashboard
+            {
+                DataContext = new DashboardViewModel()
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
